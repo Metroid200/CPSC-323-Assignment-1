3e8c4392ab10d90b7dc2724e2c75724c02b668bb
Darin Chan <dchan250@csu.fullerton.edu>
2023-03-02 01:54:56 -0800

    updated int and real fsm

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 def lexer(num):
     int_real_FSM(num)
 
+
 def int_real_FSM(num):
     states = {
-        "START": [("123456789", "INTEGER*"), ("0", "ZERO*")],
-        "INTEGER*": [("0123456789", "INTEGER*"), (".", "DOT")],
-        "ZERO*": [(".", "DOT")],
-        "DOT": [("0123456789", "SIGNIFICAND*")],
-        "SIGNIFICAND*": [("0123456789", "SIGNIFICAND*")]
+        'start': [('123456789', 'integer'), ('0', 'zero')],
+        'integer': [('0123456789', 'integer'), ('.', 'dot')],
+        'zero': [('.', 'dot')],
+        'dot': [('0123456789', 'real')],
+        'real': [('0123456789', 'real')]
     }
-    # print(states)
-    state = "START"
+    state = 'start'
     while True:
         if (len(num) > 0):
-            # print("Current state: " + state)
             token = num[0]
             num = num[1:]
             found = False
@@ -23,14 +22,19 @@ def int_real_FSM(num):
                     found = True
                     break
             if not found:
-                state = "ERROR"
+                state = 'error'
+                break
         else:
-            if "*" not in state:
-                state = "ERROR"
+            if state == 'zero':
+                state = 'integer'
+            if state != 'integer' and state != 'real':
+                state = 'error'
             break
+    # return state
     print(state)
 
 
 if __name__ == '__main__':
-    num = "0.0"
-    lexer(num)
+    nums = ['0', '5.67', '33468', '-8', '01', '67a8', '_76432', '546.42.5', '6865.453', '5.']
+    for num in nums:
+        lexer(num)
