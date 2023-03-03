911cae218149297e1f11cd272b5d0ca7531d20e7
Ricky Pena <ricardopena737@yahoo.com>
2023-03-03 14:15:47 -0800

    operator expansion

--- CPSC323-Project.py
+++ CPSC323-Project.py
@@ -1,17 +1,14 @@
-import re
 
 file = open("read.py")
 
-lexstates = ["Identifier", "Integer", "Real", "Null"]
 operators = {'=' : 'operator','+' : 'operator','-' : 'operator','/' : 'operator','==' : 'operator','%' : 'operator'
-,'*' : 'operator','<' : 'operator','>' : 'operator' }
+,'*' : 'operator','<' : 'operator','>' : 'operator', '<=' : 'operator', '>=' : 'operator' }
 operators_key = operators.keys()
 
 keyword = ['for','while','False','await','else','if','import','pass','None','break','except','in','raise','True'
 ,'class','finally','is','return','and','continue','lambda','try','as','def','from','nonlocal','assert','del'
 ,'global','not','with','async','elif','or','yield','endwhile','forend','ifend','range','print']
 
-integer = ["0", "1", "2", "3", "4", "5","6", "7", "8", "9"]
 
 punctuation_symbol = { ':' : 'colon', ';' : 'semi-colon', '.' : 'dot' , ',' : 'comma' }
 punctuation_symbol_key = punctuation_symbol.keys()
@@ -24,35 +21,6 @@ dataFlag = False
 a=file.read()
 
 
-###### Start of FSM build attempt, delete block if inoperable #########
-class myFSM:
-    def __init__(self):
-        self.handlers = {}
-    def add_state(self, name, handler):
-        self.handlers[name] = handler
-    def run(self, startingState, cargo):
-        handler = self.handlers[startingState]
-        while True:
-            (newState, cargo) = handler(cargo)
-            if newState == "END":
-                print ("END reached")
-                break
-            handler = self.handlers[newState]
-    def state_id(cargo):
-        pass
-    def state_int(cargo):
-        pass
-    def state_real(cargo):
-        pass
-lexer = myFSM()
-lexer.add_state("Identifier", state_id)
-lexer.add_state("Integer", state_int)
-lexer.add_state("Real", state_real)
-
-
-#lexer.run("Identifier", 3)
-
-######### end of FSM build attempt #############
 program = a.split("\n")
 print("lexems ---------- token\n")
 for line in program:
